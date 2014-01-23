cf889a0b49f24ab2cce674cfcdcfa4c2fddfeb1e
rully ramanda <krooco@gmail.com>
2014-01-23 12:34:32 +0700

    some more refactoring (again)

--- index.php
+++ index.php
@@ -5,6 +5,8 @@ define('DEBUG', TRUE);
 
 require_once 'wy_files/libs/AltoRouter.php';
 require_once 'wy_files/cores/Container.php';
+require_once 'wy_files/cores/Database.php';
+require_once 'wy_files/cores/Controller.php';
 $appConfig = require_once 'wy_files/confs/app.php';
 $routeCollections = require_once 'wy_files/confs/routes.php';
 
@@ -12,12 +14,16 @@ $altoRouter = new AltoRouter($routeCollections, dirname($_SERVER['SCRIPT_NAME'])
 
 $theContainer = new Container($appConfig);
 
+$theContainer['db_conn'] = $theContainer->share(function ($c) {
+    return new Database($c['db']['dsn'], $c['db']['username'], $c['db']['password']);
+});
+
 $matchRoute = $altoRouter->match();
 
 $theContainer['router'] = $altoRouter;
 
 if($matchRoute['target'] !== NULL){
-    $target = explode(':', $matchRoute);
+    $target = explode(':', $matchRoute['target']);
     if(count($target) >= 3){
         $moduleName = $target[0];
         $controllerName = ucfirst($target[1]).'Controller';
--- wy_files/controllers/PageController.php
+++ wy_files/controllers/PageController.php
@@ -3,7 +3,7 @@
 class PageController extends Controller{
     
     public function index(){
-        $categories = 
+        $categories = $this->container['db_conn']->query('select * from wy_category');
         
         foreach($categories as $category){
             echo $category->cat_title;
