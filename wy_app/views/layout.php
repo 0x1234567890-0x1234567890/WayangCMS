6e5c687f78446a7d9341240cd083b12036809e52
junior <root@junior>
2014-03-24 20:04:26 +0700

    Add Image Uploader core and controller

--- wy_app/views/layout.php
+++ wy_app/views/layout.php
@@ -12,10 +12,10 @@
         <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
         <!-- Favicons
             ================================================== -->
-        <link rel="shortcut icon" href="img/favicon.ico">
-        <link rel="apple-touch-icon" href="img/apple-touch-icon.png">
-        <link rel="apple-touch-icon" sizes="72x72" href="img/apple-touch-icon-72x72.png">
-        <link rel="apple-touch-icon" sizes="114x114" href="img/apple-touch-icon-114x114.png">
+        <link rel="shortcut icon" href="<?php echo WY_Request::base_url(); ?>/assets/images/icon.ico">
+        <link rel="apple-touch-icon" href="<?php echo WY_Request::base_url(); ?>/assets/images//apple-touch-icon.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="<?php echo WY_Request::base_url(); ?>/assets/images//apple-touch-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="<?php echo WY_Request::base_url(); ?>/assets/images//apple-touch-icon-114x114.png">
         <!-- JS
             ================================================== -->
         <script src="<?php echo WY_Request::base_url(); ?>/assets/theme/js/jquery.js" type="text/javascript"></script>
