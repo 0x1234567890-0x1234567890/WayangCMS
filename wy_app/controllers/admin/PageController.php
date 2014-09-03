5489752b08bad42e2cda129ed045f35b2013ddd2
junior <junior@junior-PC>
2014-09-03 22:14:36 +0700

    fix page without tags add tags list input --not fixed update table wy_page add column taglist

--- wy_app/controllers/admin/PageController.php
+++ wy_app/controllers/admin/PageController.php
@@ -34,7 +34,6 @@ class PageController extends WY_TController
             $content = $_POST['content'];
             $isParent = $_POST['isParent'];
             $tags = $_POST['taglist'];
-            var_dump($tags);
             $permalink = strtolower(str_replace(' ', '-', $title));
             WY_Db::execute('INSERT INTO `wy_page`'
                     . '(`author`, `title`, `date_add`, `content`, `comment_open`, `published`, `use_plugin`, `is_parent`, `permalink`, `taglist`) '
@@ -50,7 +49,7 @@ class PageController extends WY_TController
                     ':permalink'=>$permalink,
                     ':taglist'=>$tags,
                 ));
-            //WY_Response::redirect('admin/pages/all');
+            WY_Response::redirect('admin/pages/all');
         }
         $isParent = WY_Db::all('SELECT * FROM wy_page WHERE is_parent = 0');
         $this->layout->pageTitle = 'Wayang CMS - Pages Add';
@@ -86,7 +85,6 @@ class PageController extends WY_TController
             $isParent = $_POST['isParent'];
             $content = $_POST['content'];
             $tags = $_POST['taglist'];
-            var_dump($_POST['taglist']);
             $permalink = strtolower(str_replace(' ', '-', $title));
             WY_Db::execute('UPDATE wy_page 
                 SET title = :title, date_modified = NOW(), content = :content, published = :published, is_parent= :is_parent, permalink = :permalink, comment_open = :comment_open, taglist = :taglist WHERE page_id = :id', array(
@@ -99,7 +97,7 @@ class PageController extends WY_TController
                     ':taglist'=>$tags,
                     ':id'=> (int) $id,
                 ));
-           // WY_Response::redirect('admin/pages/all');
+            WY_Response::redirect('admin/pages/all');
         }
         $this->layout->pageTitle = 'Wayang CMS - Pages Edit';
         $this->layout->content = WY_View::fetch('admin/pages/edit',array('page'=>$page, 'isParent'=>$isParent));
