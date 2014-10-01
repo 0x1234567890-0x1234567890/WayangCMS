9a97bfc32ec778a6bcd494605803c8e883e39513
rully ramanda <krooco@gmail.com>
2014-10-01 12:39:06 +0700

    fixing wrong class name

--- wy_app/controllers/admin/HomeController.php
+++ wy_app/controllers/admin/HomeController.php
@@ -15,10 +15,10 @@ class HomeController extends WY_TController
     public function index()
     {
         self::auth();
-        $pages=wy_db::row("SELECT COUNT(*) as num FROM wy_pages");
-        $posts=wy_db::row("SELECT COUNT(*) as num FROM wy_posts");
-        $comments=wy_db::row("SELECT COUNT(*) as num FROM wy_comments");
-        $users=wy_db::row("SELECT COUNT(*) as num FROM wy_users");
+        $pages = WY_Db::row("SELECT COUNT(*) as num FROM wy_pages");
+        $posts = WY_Db::row("SELECT COUNT(*) as num FROM wy_posts");
+        $comments = WY_Db::row("SELECT COUNT(*) as num FROM wy_comments");
+        $users = WY_Db::row("SELECT COUNT(*) as num FROM wy_users");
         $this->layout->pageTitle = 'Wayang CMS - Dashboard';
         $this->layout->content = WY_View::fetch('admin/home/statistic',array("pages"=>$pages,"posts"=>$posts,"comments"=>$comments,"users"=>$users));
     }
